47f4acda76bd5ed927cf57138b001bbd7e1b0516
Xin He <pkuimhexin@gmail.com>
2025-02-14 20:51:13 +0800

    Backend: Create API

--- Client/backend/app.py
+++ Client/backend/app.py
@@ -1,11 +1,27 @@
 from flask import Flask, jsonify
-
+import json
 app = Flask(__name__)
 
 # 创建测试路由，前端将会访问此接口测试连通性
 @app.route('/test', methods=['GET'])
 def test():
     return jsonify(message="Hello from Flask NEW!")
+
+@app.route('/patient/<path:pid>', methods=['GET'])
+def get_data(pid):
+    if pid:
+        with open(f'data/patient/patient{pid}_visit.json', 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            return jsonify(data)
+    else:
+        return jsonify({"error": "Invalid filename"}), 400
+
+@app.route('/table', methods=['GET'])
+def get_table():
+    with open('data/herb/Table.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
