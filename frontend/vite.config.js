c76d76894f424e831e6aa98775452d25470f680a
TSBB2012nc <1746974162@qq.com>
2025-03-27 20:51:30 +0800

    v0.1 update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/VisLab-TCMRecords/",
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  }
+  plugins: [vue()]
 });
